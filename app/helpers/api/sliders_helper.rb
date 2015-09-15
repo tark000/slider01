4c0f0c9bbfbb30247e8ec354b99d45a66c8b25aa
taras <tark000@ukr.net>
2015-09-16 01:24:09 +0300

    1

---
+++ app/helpers/api/sliders_helper.rb
@@ -0,0 +1,2 @@
+module Api::SlidersHelper
+end
---
+++ test/controllers/api/sliders_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::SlidersControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
